d8124c1dd1547c025c1858d87b51af5a7d55af31
vbalaguera <vphbalaguera@gmail.com>
2026-02-15 20:00:20 +0100

    update main.py before live testing

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import books_router, places_router, products_router
 
 app = FastAPI(title="Satellite API")
+
+# Add this block!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, you'll change this to your React URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(books_router)
 app.include_router(places_router)
